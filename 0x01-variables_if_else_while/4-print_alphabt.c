9fd73fb7138757539ae5ecac51ad32a20ee730a0
TutTrue <hamdy.xd55@gmail.com>
2023-03-17 14:27:16 -0700

    Task 4 try 3

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,19 +4,19 @@
  * main - Print from a to z except q and e
  *
  * Return: Always 0.
-*/
+ */
 int main(void)
 {
-        char ch;
-        for (ch = 'a'; ch <= 'z'; ch++)
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		if (ch == 'q' || ch == 'e')
 			continue;
 
-                putchar(ch);
+		putchar(ch);
 	}
 
-        putchar('\n');
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
