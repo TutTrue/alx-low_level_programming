0a820d189bea3b49e3d7d0c8a92531e993019531
TutTrue <hamdy.xd55@gmail.com>
2023-03-21 13:34:19 -0700

    Task 9 try 2

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,13 +5,16 @@
  */
 void times_table(void)
 {
-	int c , r;
+	int c, r;
 
 	for (c = 0; c < 10; c++)
 	{
-		for (r = 0; r < 10; r++)
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (r = 1; r < 10; r++)
 		{
-			if((r * c) > 9)
+			if ((r * c) > 9)
 			{
 				_putchar(((r * c) / (10)) + '0');
 				_putchar(((r * c) % (10)) + '0');
