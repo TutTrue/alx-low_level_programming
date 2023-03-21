42e930f4b74fdc5f3fc0861b42c8d429865b6e3c
TutTrue <hamdy.xd55@gmail.com>
2023-03-21 13:40:12 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - Take 2 numbers add them and return the putput.
+ * @n1: The first number.
+ * @n2: The second number.
+ * Return: The sum of them.
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
