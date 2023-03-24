2cd3d42aa730493d14f8eda23d5a231ee04b22f7
TutTrue <hamdy.xd55@gmail.com>
2023-03-24 06:07:14 -0700

    Task 6 try 4

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,16 +1,15 @@
-#include "test/main.h"
+#include "main.h"
 
-/*
- * print_line - Print n time _ if n > 0 else print '\n'.
- * @n: The numbers of _.
- * Return: void.
+/**
+ * print_line - print underscores
+ * @n: number of _
+ * Return: void
  */
 void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 1; i <= n; i++)
 		_putchar('_');
-
 	_putchar('\n');
 }
