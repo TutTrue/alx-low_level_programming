1c1a0536194ef9e61de3fe91f0ebfbc8192b9c06
TutTrue <hamdy.xd55@gmail.com>
2023-03-24 05:51:22 -0700

    Task 6 try 2

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,17 +6,10 @@
  */
 void print_line(int n)
 {
-	if (n > 0)
-	{
-		int i;
+	int i;
 
-		for (i = 0; i <= n; i++)
-			_putchar('_');
+	for (i = 0; i < n; i++)
+		_putchar('_');
 
-		_putchar('\n');
-	}
-	else
-	{
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
