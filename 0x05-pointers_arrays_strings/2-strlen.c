307e147aec61c84adde06519d95ae988d65e116b
TutTrue <hamdy.xd55@gmail.com>
2023-03-27 09:28:02 -0700

    Task 2 and 4 try 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,8 +4,9 @@
 /**
  * _strlen -  Returns the length of a string.
  *@s: The string it returns a length of.
+ *Return: The length of string.
  */
 int _strlen(char *s)
 {
-	return (int)strlen(s);
+	return ((int)strlen(s));
 }
