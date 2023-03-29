2725135b477fc6290ea84e9219648278e81e16ef
TutTrue <hamdy.xd55@gmail.com>
2023-03-29 14:45:51 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,10 +12,9 @@
 char *_strcat(char *dest, char *src)
 {
 	int len1 = strlen(dest);
-	int len2 = strlen(src);
 	int i;
 
-	for (i = 0; i < len2; i++)
+	for (i = 0; src[i] != '\0'; i++)
 		dest[len1++] = src[i];
 
 	dest[len1] = '\0';
