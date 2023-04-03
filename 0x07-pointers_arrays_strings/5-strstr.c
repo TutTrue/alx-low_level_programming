7ea8c6bf29c168fbd2e17d57585b421a6df448c2
TutTrue <hamdy.xd55@gmail.com>
2023-04-03 08:53:27 -0700

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -5,31 +5,28 @@
 /**
  * _strstr - Finds the first occurrence of the substring needle in the string haystack.
  * The terminating null bytes (\0) are not compared
- * 
  * @haystack: The string that will be scanned.
  * @needle: This is the small string to be searched with-in haystack string.
- * 
  * Return: A pointer to the beginning of the located substring,
  * or NULL if the substring is not found.
 */
 char *_strstr(char *haystack, char *needle)
 {
-    unsigned int i, j = 0;
-    unsigned int len = strlen(needle);
+	unsigned int i, j = 0;
+	unsigned int len = strlen(needle);
 
-    while (haystack[j] != '\0')
-    {
-        for (i = 0; needle[i] != '\0'; i++)
-        {
-            if (needle[i] != *haystack)
-                break;
-        }
-        
-        if (len == i)
-            return (haystack);
+	while (haystack[j] != '\0')
+	{
+		for (i = 0; needle[i] != '\0'; i++)
+		{
+			if (needle[i] != *haystack)
+				break;
+		}
+		
+		if (len == i)
+			return (haystack);
 
-        j++;
-    }
-    return (NULL);
-
+		j++;
+	}
+	return (NULL);
 }
