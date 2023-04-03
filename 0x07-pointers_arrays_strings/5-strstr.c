941f9e44c0e62ab6b52555959f414832a87151ef
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2023-04-03 17:03:45 +0200

    Task 5 try 7

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,10 +14,10 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
-    unsigned int i;
+    unsigned int i, j = 0;
     unsigned int len = strlen(needle);
 
-    while (*haystack)
+    while (haystack[j] != '\0')
     {
         for (i = 0; needle[i] != '\0'; i++)
         {
@@ -28,7 +28,7 @@ char *_strstr(char *haystack, char *needle)
         if (len == i)
             return (haystack);
 
-        haystack++;
+        j++;
     }
     return (NULL);
 
