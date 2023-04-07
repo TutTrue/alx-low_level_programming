53f720f6b45332d9f8b2e751ef117e63ec838541
TutTrue <hamdy.xd55@gmail.com>
2023-04-07 14:17:54 -0700

    replace 3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _puts -  Prints a string, followed by a new line, to stdout.
@@ -6,10 +7,5 @@
  */
 void _puts(char *str)
 {
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-		_putchar(str[i]);
-
-	_putchar('\n');
+	puts(str);
 }
