af5939ee30b4e84713aeda7e37751334efab5f1c
TutTrue <hamdy.xd55@gmail.com>
2023-04-11 13:56:13 -0700

    Task 2 correct output case

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,21 +12,18 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, len1, len2;
+	int i, j, len1 = 0, len2 = 0;
 	char *p;
 
-	if (s1 == NULL)
-		s1 = "";
-	else if (s2 == NULL)
-		s2 = "";
+	if (s1 != NULL)
+		len1 = strlen(s1);
+	else if (s2 != NULL)
+		len2 = strlen(s2);
 
-	p = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
+	p = malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (p == NULL)
 		return (NULL);
-
-	len1 = (int) strlen(s1);
-	len2 = (int) strlen(s2);
 
 	for (i = 0; i < len1; i++)
 		p[i] = s1[i];
