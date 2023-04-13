f0f761c782c28ef583f5e9f6f50d0e71de146575
TutTrue <hamdy.xd55@gmail.com>
2023-04-13 14:10:41 -0700

    Task 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -35,9 +35,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (nptr == NULL)
 		return (NULL);
 
-	for (i = 0; i < old_size && i < new_size; i++)
+	for (i = 0; i < (int) old_size && i < (int) new_size; i++)
 	{
-		nptr[i] = ptr[i];
+		nptr[i] = ((char *) ptr)[i];
 	}
 	free(ptr);
 	return (nptr);
