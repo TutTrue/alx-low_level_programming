e087b9c2b6abb1caa703b070356bb8504e8c1e81
TutTrue <hamdy.xd55@gmail.com>
2023-04-17 09:12:47 -0700

    Task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "dog.h"
+#include <string.h>
+
+/**
+ * new_dog -  creates a new dog.
+ * @name: dog name
+ * @age: dogage
+ * @owner: dog owner
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t newDog = malloc(sizeof(dog_t));
+	if (newDog == NULL)
+		return (NULL);
+	if (name == NULL)
+	{
+		free(owner);
+		free(newDog);
+		return (NULL);
+	}
+	if (owner == NULL)
+	{
+		free(name);
+		free(newDog);
+		return (NULL);
+	}
+	newDog->name = name;
+	newDog->owner = owner;
+	newDog->age = age;
+	return (newDog);
+}
