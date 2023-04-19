57b2bf04b1cf0e83be68b3305bceeb3707a65f5d
TutTrue <hamdy.xd55@gmail.com>
2023-04-19 02:48:48 -0700

    Task 2 return -1 if NULL

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,7 +14,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, x;
 
-	if (array == NULL || action == NULL)
+	if (array == NULL || cmp == NULL)
 		return (-1);
 
 	if ((int) size <= 0)
