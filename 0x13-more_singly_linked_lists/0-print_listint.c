7a0777b164d96ab6f40237f3944f50bc35b08a72
TutTrue <hamdy.xd55@gmail.com>
2023-04-28 05:43:25 -0700

    Task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - print linked list
+ * @h: head of the linked list
+ * Return: number of nodes in list
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *p;
+	int counter = 0;
+
+	p = h;
+	while (!p)
+	{
+		printf("%d\n", p->n);
+		counter++;
+		p = p->next;
+	}
+	return (counter);
+}
