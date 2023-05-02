a5d5204b93d80e5e24bcd83896ea671eedc0b783
TutTrue <hamdy.xd55@gmail.com>
2023-05-02 14:31:29 -0700

    Task 7 check if head is null

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,9 +8,12 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *p = head;
+	listint_t *p;
 	unsigned int i;
 
+	if (!head)
+		return (NULL);
+	p = head;
 	if (index == 0)
 		return (p);
 	for (i = 1; i <= index; i++)
