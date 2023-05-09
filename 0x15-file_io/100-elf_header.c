be1bc5092a1f2a05037dcf5f5236a990a50a819e
TutTrue <hamdy.xd55@gmail.com>
2023-05-09 13:05:52 -0700

    Task 4 betty

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 		write(STDERR_FILENO, "Usage: elf_header elf_filename\n", 31), exit(98);
-	
+
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 		write(STDERR_FILENO, "Error: Can't read from file\n", 28), exit(98);
