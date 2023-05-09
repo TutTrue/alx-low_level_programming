75766e2ffacf0ff3cc96b7dcd971cb0db2678088
TutTrue <hamdy.xd55@gmail.com>
2023-05-09 12:52:53 -0700

    Task 4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 		write(STDERR_FILENO, "Usage: elf_header elf_filename\n", 31), exit(98);
 	
-	fd = open(argc[1], O_RDONLY);
+	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 		write(STDERR_FILENO, "Error: Can't read from file\n", 28), exit(98);
 	exit(98);
