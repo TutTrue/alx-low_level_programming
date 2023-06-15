c0cb49b56a23eeda434b25c91e3fd278b2ee8c04
TutTrue <hamdy.xd55@gmail.com>
2023-06-15 11:28:24 -0700

    Task 8 still trying

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,7 +12,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	for (i = 0; i < index && cur; i++)
 		cur = cur->next;
-	if (!cur || !head)
+	if (!cur)
 		return (-1);
 	if (cur == *head)
 	{
