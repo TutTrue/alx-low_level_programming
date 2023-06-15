929cd1f541967b8c58f14cd72ee93464646f88c2
TutTrue <hamdy.xd55@gmail.com>
2023-06-15 10:05:18 -0700

    task 8, task 5 and 7 fix bug

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (!new_node)
 		return (NULL);
 	new_node->n = n;
-	if (idx == 0 || !*h)
+	if (idx == 0)
 	{
 		free(new_node);
 		return (add_dnodeint(h, n));
---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - delete a specific node at given index
+ * @head: pointer to the head of the node
+ * @index: index of the node that will be deleted
+ * Return: 1 on success -1 on fail
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *cur = *head;
+	unsigned int i;
+
+	for (i = 0; i < index; i++)
+	{
+		if (!cur)
+			return (-1);
+		cur = cur->next;
+	}
+	if (!cur)
+		return (-1);
+	if (cur == *head)
+	{
+		(*head) = (*head)->next;
+		free(cur);
+		return (1);
+	}
+	if (!cur->next)
+	{
+		free(cur);
+		return (1);
+	}
+	cur->next->prev = cur->prev;
+	cur->prev->next = cur->next;
+	free(cur);
+	return (1);
+
+}
