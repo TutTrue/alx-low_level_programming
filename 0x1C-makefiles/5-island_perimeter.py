c459b9e9b1e307bc09e2757598418c6cc2be466e
MahmoudHadmy <hamdy.xd55@gmail.com>
2023-08-23 23:50:28 +0300

    3 -> 5 and the advanced task

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""returns the perimeter of the island described in grid"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid"""
+    per = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                if grid[i - 1][j] == 0:
+                    per += 1
+                if grid[i][j + 1] == 0:
+                    per += 1
+                if grid[i + 1][j] == 0:
+                    per += 1
+                if grid[i][j - 1] == 0:
+                    per += 1
+    return per
