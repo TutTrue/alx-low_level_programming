374235cdf8e55dc936815cb93e8dd4ff655ed847
MahmoudHamdy <hamdy.xd55@gmail.com>
2023-10-11 17:12:27 +0300

    fix betty style

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -8,38 +8,39 @@
 */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t s = 0, e = size - 1, m;
+	size_t s = 0, e = size - 1, m;
 
-    if (!array)
-        return (-1);
-    while (e >= s)
-    {
-        m = (e + s) / 2;
-        printf("Searching in array: ");
-        print_arr(array, s, e);
-        if (array[m] == value)
-            return (m);
-        if (array[m] > value)
-            e = m - 1;
-        if (array[m] < value)
-            s = m + 1;
-    }
-    return (-1);
+	if (!array)
+		return (-1);
+	while (e >= s)
+	{
+		m = (e + s) / 2;
+		printf("Searching in array: ");
+		print_arr(array, s, e);
+		if (array[m] == value)
+			return (m);
+		if (array[m] > value)
+			e = m - 1;
+		if (array[m] < value)
+			s = m + 1;
+	}
+	return (-1);
 }
 /**
  * print_arr - print a partition of an array
+ * @array: the array we print
  * @s: start index
  * @e: end index
 */
 void print_arr(int *array, size_t s, size_t e)
 {
-    size_t i;
+	size_t i;
 
-    for (i = s; i <= e; i++)
-    {
-         if (i != s)
-            printf(", ");
-         printf("%d", array[i]);
-    }
-    printf("\n");
+	for (i = s; i <= e; i++)
+	{
+		if (i != s)
+			printf(", ");
+		 printf("%d", array[i]);
+	}
+	printf("\n");
 }
